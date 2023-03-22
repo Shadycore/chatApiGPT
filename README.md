2c91c9e3f07ae8ecaa8472dc4d05b604594e1f71
Shadycore <walter.lopez.calle@hotmail.com>
2023-03-21 23:12:08 -0500

    Listo

---
+++ README.md
@@ -0,0 +1,11 @@
+Programa de consola que permite interactuar a través de un chat con GPT-3.
+Debe generar tu api key y reemplazarla en el archivo config.json, también puedes cambiar el modelo a usar.
+
+MODELO				DESCRIPCIÓN
+text-davinci-003	El modelo GPT-3 más capaz. Puede realizar cualquier tarea que puedan realizar los otros modelos, 
+					a menudo con mayor calidad, mayor duración y mejor seguimiento de instrucciones. 
+					También admite la inserción de finalizaciones dentro del texto.	4,000 tokens
+text-curie-001		Muy capaz, pero más rápido y de menor costo que Davinci.
+text-babbage-001	Capaz de realizar tareas sencillas, muy rápidas y de menor costo.
+text-ada-001	    Capaz de tareas muy simples, generalmente el modelo más rápido de la serie GPT-3 y el costo más bajo.
+URL: https://beta.openai.com/docs/models/gpt-3
---
+++ chatapigpt.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+class Chatapigpt
+{
+    static void Main(string[] args)
+    {
+
+        //tomo los datos de la configuración
+        var config = JObject.Parse(File.ReadAllText("config.json"));
+        var apiKey = config["api_key"].ToString();
+        var model = config["model"].ToString();
+        var maxTokens = config["max_tokens"];
+        var temperature = config["temperature"];
+
+        //Conexión a la api de GPT 3
+        var client = new RestClient("https://api.openai.com/v1");
+        var request = new RestRequest("/completions", Method.Post);
+
+        // Aquí debe agregar las credenciales de su API de GPT-3
+        request.AddHeader("Authorization", "Bearer " + apiKey);
+        request.AddHeader("Content-Type", "application/json");
+        Console.WriteLine("Para terminar de interacturar escribir: SALIR \n");
+        Console.WriteLine("\u001b[37mYo: "); //Color blanco para las preguntas
+        var texto = Console.ReadLine();
+        while (texto != "SALIR")
+        {
+            // Crea el objeto de la solicitud
+            var requestObject = new { model = model, prompt = texto, max_tokens = maxTokens, temperature = temperature };
+            // Convierte el objeto en una cadena JSON
+            var jsonString = JsonConvert.SerializeObject(requestObject);
+            // Agrega la cadena JSON como parámetro a la solicitud
+            request.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+
+            // Envía la solicitud a la API
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                // Imprime la respuesta de GPT-3 
+                var data = JObject.Parse(response.Content);
+                var choices = data["choices"].First;
+                var textgpt = choices["text"].ToString();
+                Console.WriteLine("\u001b[32mGPT-3: " + textgpt); //color verde para las respuestas
+            }
+            else
+            {
+                Console.WriteLine("Error al conectarse a la API de GPT-3: " + response.Content);
+            }
+
+            // Espera la entrada del usuario para el siguiente mensaje
+            Console.Write("\u001b[37mYo: ");
+            texto = Console.ReadLine();
+        }
+
+
+    }
+}
